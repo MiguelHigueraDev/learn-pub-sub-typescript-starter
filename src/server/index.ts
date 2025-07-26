b57f1f14ea73900cc61c902b4ff519a8f04a0fed
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2025-07-26 10:16:42 -0400

    connect to RabbitMQ

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,26 @@
+import amqp from "amqplib";
+
+const RABBITMQ_URL = "amqp://guest:guest@localhost:5672/";
+
+let connection: amqp.ChannelModel | null = null;
+
 async function main() {
-  console.log("Starting Peril server...");
+  connection = await amqp.connect(RABBITMQ_URL);
+  console.log("Connected to RabbitMQ");
 }
+
+process.on("SIGINT", async () => {
+  if (connection) {
+    try {
+      await connection.close();
+      console.log("RabbitMQ connection closed");
+    } catch (err) {
+      console.error("Error closing RabbitMQ connection:", err);
+    }
+  }
+
+  process.exit(0);
+});
 
 main().catch((err) => {
   console.error("Fatal error:", err);
